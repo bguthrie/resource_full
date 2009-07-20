c7fe10fecd7e2666b535c9730b1322c336c3fc8d
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2009-07-20 12:50:45 -0500

    Added new stubbed methods to inject options for all CRUD methods for the xml and json format renderers. These can be overridden for any specific needs rather than overriding the whole outer method.

--- lib/resource_full/render/json.rb
+++ lib/resource_full/render/json.rb
@@ -2,27 +2,36 @@ module ResourceFull
   module Render
     module JSON
       protected
-  
+
       def json_class_name(obj)
         obj.class.name.demodulize.underscore
       end
-  
+
+      def show_json_options
+        {}
+      end
       def show_json
         self.model_object = send("find_#{model_name}")
-          render :json => model_object.to_json
+          render :json => model_object.to_json(show_json_options)
       rescue ActiveRecord::RecordNotFound => e
         render :json => e.to_json , :status => :not_found
       end
 
+      def index_json_options
+        {}
+      end
       def index_json
         self.model_objects = send("find_all_#{model_name.pluralize}")
-        render :json => model_objects.to_json
+        render :json => model_objects.to_json(index_json_options)
       end
 
+      def create_json_options
+        {}
+      end
       def create_json
         self.model_object = send("create_#{model_name}")
         if model_object.errors.empty?
-          render :json => model_object.to_json, :status => :created, :location => send("#{model_name}_url", model_object.id)
+          render :json => model_object.to_json(create_json_options), :status => :created, :location => send("#{model_name}_url", model_object.id)
         else
           json_data = model_object.attributes
           json_data[:errors] = {:list => model_object.errors,
@@ -31,10 +40,13 @@ module ResourceFull
         end
       end
 
+      def update_json_options
+        {}
+      end
       def update_json
-        self.model_object = send("update_#{model_name}")      
+        self.model_object = send("update_#{model_name}")
         if model_object.errors.empty?
-          render :json => model_object.to_json
+          render :json => model_object.to_json(update_json_options)
         else
           json_data = model_object.attributes
           json_data[:errors] = {:list => model_object.errors,
@@ -51,9 +63,12 @@ module ResourceFull
       rescue ActiveRecord::RecordNotFound => e
         render :json => e.to_json , :status => :not_found
       end
-  
+
+      def new_json_options
+        {}
+      end
       def new_json
-        render :json => send("new_#{model_name}").to_json
+        render :json => send("new_#{model_name}").to_json(new_json_options)
       end
     end
   end
