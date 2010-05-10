9bb61faadf98a1ba94c4ed7ad8b59cb90d760c3e
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2010-05-10 13:13:52 -0500

    Vijay: Resurrecting ResourceFull::Base for a short time - till we can move all our code to use the inclusion paradigm.

---
+++ lib/resource_full/base.rb
@@ -0,0 +1,7 @@
+# @deprecated
+# This class is present only for backwards compatibility and will be removed within the next few weeks.
+module ResourceFull
+  class Base < ActionController::Base
+    include ResourceFull
+  end
+end
