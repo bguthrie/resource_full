ff8db69450a5f430089d885613ede6ed792017cb
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2010-05-10 13:40:09 -0500

    Vijay: Adding missing require for ResourceFull::Base

--- lib/resource_full.rb
+++ lib/resource_full.rb
@@ -19,10 +19,11 @@ require File.dirname(__FILE__) + '/resource_full/render/xml'
 require File.dirname(__FILE__) + '/resource_full/render'
 require File.dirname(__FILE__) + '/resource_full/retrieve'
 require File.dirname(__FILE__) + '/resource_full/version'
+require File.dirname(__FILE__) + '/resource_full/base'
 
 module ResourceFull
   class ResourceNotFound < Exception; end
-  
+
   def model_name; self.class.model_name; end
   def model_class; self.class.model_class; end
 
@@ -35,7 +36,7 @@ module ResourceFull
         controller_class.ancestors.include?(self)
       end
     end
-    
+
     # Returns the controller for the given resource.
     def controller_for(resource)
       return resource if resource.is_a?(Class) && resource.ancestors.include?(ActionController::Base)
@@ -43,40 +44,40 @@ module ResourceFull
     rescue NameError
       raise ResourceFull::ResourceNotFound, "not found: #{resource}"
     end
-    
+
     private
-    
+
       def included(controller)
         controller.send :extend, ClassMethods
-        controller.send :include, 
-          ResourceFull::Retrieve, 
-          ResourceFull::Query, 
-          ResourceFull::Dispatch, 
+        controller.send :include,
+          ResourceFull::Retrieve,
+          ResourceFull::Query,
+          ResourceFull::Dispatch,
           ResourceFull::Render
-        
+
         if ActionPack::VERSION::STRING < "2.3.0"
           controller.session :off, :if => lambda { |request| request.format.xml? || request.format.json? }
         end
       end
   end
-  
-  
+
+
   module ClassMethods
     attr_accessor_with_default :paginatable, true
     attr_accessor_with_default :resource_identifier, :id
-    
+
     # Returns true if this resource is paginatable, which is to say, it recognizes and honors
     # the :limit and :offset parameters if present in a query.  True by default.
     def paginatable?; paginatable; end
-    
+
     # The name of the model exposed by this resource.  Derived from the name of the controller
     # by default.  See +exposes+.
     def model_name
       @model_class ? @model_class.name.demodulize.underscore : self.controller_name.singularize
     end
-    
+
     # Indicates that this resource is identified by a database column other than the default
-    # :id.  
+    # :id.
     # TODO This should honor the model's primary key column but needn't be bound by it.
     # TODO Refactor this.
     # TODO Improve the documentation.
@@ -90,10 +91,10 @@ module ResourceFull
           if opts[:unless] == :id_numeric
             opts[:unless] = lambda { |id| id =~ /^[0-9]+$/ }
           end
-          
+
           # Negate the condition to generate an :if from an :unless.
           condition = opts[:if] || lambda { |id| not opts[:unless].call(id) }
-          
+
           self.resource_identifier = lambda do |id|
             if condition.call(id)
               column
@@ -106,7 +107,7 @@ module ResourceFull
         raise ArgumentError, "identified_by expects either a block or a column name and some options"
       end
     end
-    
+
     # The class of the model exposed by this resource.  Derived from the model name.  See +exposes+.
     def model_class
       @model_class ||= model_name.camelize.constantize
@@ -117,7 +118,7 @@ module ResourceFull
     def exposes(model_class)
       @model_class = model_class.to_s.singularize.camelize.constantize
     end
-    
+
     # Renders the resource as XML.
     def to_xml(opts={})
       { :name       => self.controller_name,
@@ -125,13 +126,13 @@ module ResourceFull
         :identifier => self.xml_identifier
       }.to_xml(opts.merge(:root => "resource"))
     end
-    
+
     protected
-    
+
       def xml_identifier
         (self.resource_identifier.is_a?(Proc) ? self.resource_identifier.call(nil) : self.resource_identifier).to_s
       end
-        
+
   end
 end
 
